6a5b790335113702bc32b444365d96fde60a0ea0
Edwin1510 <143858534+Edwin1510@users.noreply.github.com>
2023-11-04 19:08:21 +0530

    Add files via upload

---
+++ json.js
@@ -0,0 +1,60 @@
+// JSON RESUME
+let myResume={
+    "basics": {
+      "name": "edwin",
+      "email": "edwinedwin1510@gmail.com",
+      "phone": 8973348346,
+      "degree": "B.E",
+      "location": {
+        "address": "13/35,palpannaicolony,samayanallur",
+        "postalCode": 625402,
+        "city": "madurai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "github":"https://github.com/Edwin1510"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "TAFE",
+        "position": "engineer",
+        "startDate": "2022-11-15",
+        "endDate": "2023-11-15",
+        "summary": "i am solving inbound in sap",
+      },
+    ],
+    "education": [
+      {
+        "institution": "FATIMA MICHEAL COLLEGE OF ENG",
+        "department": "ECE",
+        "studyType": "fulltime",
+        "batch start year": 2013,
+        "batch end year": 2017,
+        "gpa": 6.5,
+      }
+    ],
+    "skills": [
+      {
+        "name": "javascript",
+        "level": "beginer",
+        "project": [
+          "cotekata pratice"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "cricket,kabati",
+      }
+    ]
+  }
+  console.log(myResume);
